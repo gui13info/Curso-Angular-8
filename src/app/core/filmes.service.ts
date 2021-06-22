ef9e83059f2d3ce31804a74907b844a6dcf39f68
Guilherme Marques <marques.guilherme0067@gmail.com>
2021-06-22 20:46:09 -0300

    Service filmes

--- src/app/core/filmes.service.ts
+++ src/app/core/filmes.service.ts
@@ -1,21 +1,21 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { Filme } from '../shared/models/filme';
-import { ConfigPrams } from '../shared/models/config-prams';
-import { ConfigParamsService } from './config-params.service';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
 
-const url = 'http://localhost:3001/filmes/';
+import { Filme } from "../shared/models/filme";
+import { ConfigPrams } from "../shared/models/config-prams";
+import { ConfigParamsService } from "./config-params.service";
+
+const url = "http://localhost:3001/filmes/";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class FilmesService {
-
   constructor(
     private http: HttpClient,
     private configService: ConfigParamsService
-  ) { }
+  ) {}
 
   salvar(filme: Filme): Observable<Filme> {
     return this.http.post<Filme>(url, filme);
@@ -27,7 +27,7 @@ export class FilmesService {
 
   listar(config: ConfigPrams): Observable<Filme[]> {
     const configPrams = this.configService.configurarParametros(config);
-    return this.http.get<Filme[]>(url, {params: configPrams});
+    return this.http.get<Filme[]>(url, { params: configPrams });
   }
 
   visualizar(id: number): Observable<Filme> {
